6988b2253f4c4c316ad6385893a433318cfe9a83
Yanagi yah <macmaghuhin@gmail.com>
2017-02-19 17:06:16 +0900

    create rails directory

--- hello.rb
+++ hello.rb
@@ -21,3 +21,20 @@ tom = User.new("tom")
 tom.sayHi
 bob = User.new("bob")
 bob.sayHi
+
+class AdminUser < User
+
+    def sayHello
+        puts "Hello"
+    end
+
+    def sayHi
+        puts "hi!from admin"
+    end
+    
+end
+
+admin = AdminUser.new("tmo")
+admin.sayHello
+admin.sayHi
+# AdminUser.new.sayHello
